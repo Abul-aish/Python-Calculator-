d6a1e5b6586d9583e37269d7d68f8098bbdf2143
Abul-aish <abulaish444@gmail.com>
2025-09-17 02:25:34 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,12 @@
+print("simple calculator")
+a=float(input("Enter first number"))
+b=float(input("Enter second number"))
+op=input("Enter operator(+,-,*,/):")
+if op=="+":
+    print("Result:",a+b)
+elif op=="-":
+  	print("Result:",a-b)
+elif op=="*":
+  	print("Result:",a*b)
+elif op=="/":
+	  print("Result:",a/b)
